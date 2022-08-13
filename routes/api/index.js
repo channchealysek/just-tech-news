4d278817cf82e362b07c0d3f12e1b3f4b3953b6e
channchealy <sek.channchealy@yahoo.com>
2022-08-12 21:25:08 -0500

    add comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,9 +3,10 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
 
+const commentRoutes = require('./comment-routes');
+
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
-
-module.exports = router;
+router.use('/comments', commentRoutes);
 
 module.exports = router;
